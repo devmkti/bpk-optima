719c410d2a83867236eb8c5cb412de917ecb883f
Muhammad Iqbal Tawaqal <iqbal.tawakal@outlook.com>
2025-01-03 15:32:47 +0700

    Fixing fungsi simpan proyek, validasi nama proyek tidak boleh sama

--- app/controllers/proyek_controller.py
+++ app/controllers/proyek_controller.py
@@ -44,39 +44,43 @@ def get_proyek():
 def simpan_proyek(request):
     try:
         nama_proyek = request.form['nama_proyek']
-        deskripsi = request.form['deskripsi']
-        periode = request.form['periode_mulai']
-        jumlah_responden = request.form['jumlah_responden']
-        jumlah_kriteria = request.form['jumlah_kriteria']
-        kriteria = request.form.getlist('kriteria[]')
+        cek_nama = Proyek.query.filter_by(nama_proyek=nama_proyek).first()
+        if cek_nama:
+            return {"message": "Proyek dengan nama <b>" + nama_proyek + "</b> sudah ada, silahkan ubah nama proyek!"}, 400
+        else:
+            deskripsi = request.form['deskripsi']
+            periode = request.form['periode_mulai']
+            jumlah_responden = request.form['jumlah_responden']
+            jumlah_kriteria = request.form['jumlah_kriteria']
+            kriteria = request.form.getlist('kriteria[]')
 
-        # Pisahkan tanggal awal dan tanggal akhir
-        periode_awal, periode_akhir = periode.split(' to ')
+            # Pisahkan tanggal awal dan tanggal akhir
+            periode_awal, periode_akhir = periode.split(' to ')
 
-        # Proses data yang dikirimkan
-        proyek = Proyek(
-            nama_proyek=nama_proyek,
-            deskripsi=deskripsi,
-            periode_mulai=periode_awal,
-            periode_selesai=periode_akhir,
-            jumlah_responden=jumlah_responden,
-            jumlah_kriteria=jumlah_kriteria,
-            created_at=datetime.datetime.now()
-        )
-        
-        db.session.add(proyek)
-        db.session.flush()
-
-        # Tambahkan kriteria ke dalam database
-        for kriteria_ in kriteria:
-            kriteria_db = Kriteria(
-                id_proyek=proyek.id,
-                nama_kriteria=kriteria_
+            # Proses data yang dikirimkan
+            proyek = Proyek(
+                nama_proyek=nama_proyek,
+                deskripsi=deskripsi,
+                periode_mulai=periode_awal,
+                periode_selesai=periode_akhir,
+                jumlah_responden=jumlah_responden,
+                jumlah_kriteria=jumlah_kriteria,
+                created_at=datetime.datetime.now()
             )
-            db.session.add(kriteria_db)
+            
+            db.session.add(proyek)
+            db.session.flush()
 
-        db.session.commit()
-        return {"message": "Proyek berhasil ditambahkan", "id_proyek": proyek.id}
+            # Tambahkan kriteria ke dalam database
+            for kriteria_ in kriteria:
+                kriteria_db = Kriteria(
+                    id_proyek=proyek.id,
+                    nama_kriteria=kriteria_
+                )
+                db.session.add(kriteria_db)
+
+            db.session.commit()
+            return {"message": "Proyek berhasil ditambahkan", "id_proyek": proyek.id}, 200
     except Exception as e:
         db.session.rollback()
         return {"error": str(e)}
