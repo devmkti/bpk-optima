7365587744aaa7f6ded3625fa8afa872879d94e4
Muhammad Iqbal Tawaqal <iqbal.tawakal@outlook.com>
2025-07-18 10:02:30 +0700

    Fixing config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/optima'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dev_bpkoptima:BPKsILaRaNITypT2025@hqdukriksql05:5432/db_optima_18072025'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
